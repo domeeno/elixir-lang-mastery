25e11da91ae8baf1d5c5ee553da2e5f81d035bcf
domeeno <dominicflocea.dev@gmail.com>
2023-06-24 13:21:47 +0300

    add necessary dependencies, fonts, initial file structure

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,cjs,ts,tsx,vue}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+      },
+      fontFamily: {
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
